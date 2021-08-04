4239d875fa69c977ec59d97eb6e6442f4245664e
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-04 16:44:44 -0400

    Start writing some controller tests

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    create(:user)
+  end
+
+  test "should create a restaurant" do
+    assert_difference 'Restaurant.count'  do 
+      post :create, params: { restaurant: { name: "Burrito Boyz", seating_capacity: 200}}
+      assert_redirected_to restaurants_path
+    end
+  end
+
 end
