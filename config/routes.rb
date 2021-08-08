be62dfff0a0d83ec2a4ff7281493b47362d31298
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-08 15:43:03 -0400

    Begin work on user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :edit, :update, :destroy, :show ]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   root "restaurants#index"
   # root restaurants_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
