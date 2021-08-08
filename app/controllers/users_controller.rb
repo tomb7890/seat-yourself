bbe1ff3b3dfdd73d7e5b4e2754303aa4894fe3a4
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-08 15:28:56 -0400

    Begin work on User and Session controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_url, notice: "Signed up!"
+      redirect_to restaurants_path, notice: "Signed up!"
     else
       render "new"
     end
