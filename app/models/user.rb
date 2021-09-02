e03bc4903c50e4c5c16dd2575c654f6dd792cc92
Tom Billingsley <tom.billingsley@gmail.com>
2021-09-02 08:10:24 -0400

    Add some basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :restaurants, through: :reservations
 
+  validates :name, presence: true
+  validates :email, presence: true
+  
+
 end
