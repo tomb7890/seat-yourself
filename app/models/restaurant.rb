7cf8591d340feaeabd5d9f6e96c340a29d1a5a20
Tom Billingsley <tom.billingsley@gmail.com>
2022-03-15 10:19:01 -0400

    Set time zone for now

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,6 +10,10 @@ class Restaurant < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
+  def time_zone
+    'Eastern Time (US & Canada)'
+  end
+
 
   def available?(party_size, start_time)
     party_size <= seating_capacity - reservations.where(start_time: start_time).sum(:partysize)
