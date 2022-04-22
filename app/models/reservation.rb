54f187d3e0f965dbb4cc9908f2f31423d3802e42
Tom Billingsley <tom.billingsley@gmail.com>
2022-04-22 15:02:57 -0400

    Remove blank lines

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,17 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
-
   validates :partysize, numericality: { greater_than_or_equal_to: 1,  only_integer: true }
-
-
-  
 
   def format_time
     Time.zone =  restaurant.time_zone 
     start_time.in_time_zone.strftime('%A, %b %d %Y  at %H:%M:%S %Z %Y')
   end
-
-
 end
-
