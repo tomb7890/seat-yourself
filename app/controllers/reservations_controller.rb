ba260ef296573a09bc4ddf1c5e9da6713393029d
Tom Billingsley <tom.billingsley@gmail.com>
2022-04-26 15:36:17 -0400

    Check requested reservation for party size and opening hours

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,17 +1,17 @@
 class ReservationsController < ApplicationController
 
-before_action :load_restaurant
-before_action :ensure_logged_in, only: [:create, :destroy]
+  before_action :load_restaurant
+  before_action :ensure_logged_in, only: [:create, :destroy]
 
 
 
-def show
+  def show
     @reservation = Reservation.find(params[:id])
   end
 
 
-#ReservationsController#create 
-  def create 
+  def create
+    Time.zone = @restaurant.time_zone
     @reservation = @restaurant.reservations.build(reservation_params)
     @reservation.user = current_user
 
@@ -21,19 +21,27 @@ def show
                          params[:reservation]["start_time(4i)"].to_i,
                          params[:reservation]["start_time(5i)"].to_i)
     
+    desired_time = t.utc
+
+    unless  @restaurant.hours_ok?(desired_time)
+      redirect_to restaurant_path(@restaurant), alert: outside_normal_hours(t)
+      return
+    end
+
     desired_partysize = (reservation_params[:partysize]).to_i
 
-    if @restaurant.available?( desired_partysize, desired_time )
-      if @reservation.save
-        redirect_to restaurant_reservation_path(@restaurant, @reservation), notice: 'Reservation added.'
-      else 
-        # errors handled by create.js.erb 
-      end
-    else
-      redirect_to restaurant_path(@restaurant), alert: "Requested party size exceeds #{@restaurant.name}\'s seating capacity at #{desired_time}."
+    unless @restaurant.available?( desired_partysize, desired_time )
+      redirect_to restaurant_path(@restaurant), alert: excessive_party_size(t)
+      return
+    end
+
+    @reservation.start_time = desired_time
+    if @reservation.save
+      redirect_to restaurant_reservation_path(@restaurant, @reservation), notice: 'Reservation added.'
+    else 
+      # errors handled by create.js.erb 
     end
   end
-
 
   def destroy
     @reservation = Reservation.find(params[:id])
@@ -41,6 +49,16 @@ def show
   end
 
   private
+  def outside_normal_hours(the_time)
+    "Requested reservation at #{the_time.localtime} " + 
+      " is not within opening hours. Please choose a time between " +
+      " #{@restaurant.open_hour} and #{@restaurant.close_hour}."
+  end
+
+  def excessive_party_size(the_time)
+    "Requested party size exceeds #{@restaurant.name}\'s seating capacity at #{the_time.localtime}."
+  end
+  
   def reservation_params
     params.require(:reservation).permit(:partysize, :start_time, :restaurant_id)
   end
