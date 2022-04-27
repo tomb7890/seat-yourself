ff177fee4c921d20ef6564c250318f8659066b14
Tom Billingsley <tom.billingsley@gmail.com>
2022-04-27 07:08:37 -0400

    Test availability under varying conditions

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -6,25 +6,45 @@ class RestaurantTest < ActiveSupport::TestCase
   setup do
     @restaurant = create(:restaurant)
     Time.zone = @restaurant.time_zone
-
     @start_time =  Time.zone.local(2021, 8, 4,
                                 13,0,0 )
   end
   
+  def prepare_time_object(x) 
+    Time.zone.local(2021,8,4,x,0,0)
+  end
+    
+  test "availability at exactly the opening hour" do
+    to =  prepare_time_object(@restaurant.open_hour)  
+    assert @restaurant.hours_ok?(to)
   end
 
+  test "availability at exactly the closing hour" do
+    to = prepare_time_object( @restaurant.close_hour )
+    refute  @restaurant.hours_ok?(to)
+  end
 
-  # Restaurant#available? 
-  test "restaurant is available when party size is less than capacity" do
+  test "availability of last legitimate time" do
+    to = prepare_time_object( @restaurant.close_hour - 1 )
+    assert @restaurant.hours_ok?(to)
+  end
+
+  test "availability is denied for after hours parties" do
+    to =  prepare_time_object( @restaurant.close_hour + 1)
+    partysize = 9
+    refute @restaurant.available?(partysize, to) 
+  end 
+
+  test "availability when party size is less than capacity" do
     party_size = 10
     assert @restaurant.available?(party_size, @start_time )
   end
 
-  test "restaurant is not available when party size is greater than capacity" do
+  test "availability when party size is greater than capacity" do
     refute @restaurant.available?(@restaurant.seating_capacity + 1, @start_time )
   end
 
-  test "restaurant is available when party size is exactly capacity" do
+  test "availability when party size is exactly capacity" do
     assert @restaurant.available?(@restaurant.seating_capacity, @start_time ) 
   end
 
@@ -35,15 +55,14 @@ class RestaurantTest < ActiveSupport::TestCase
   end
 
   test "reservation isn't available when reservations + party size > seating_capacity" do
-    reservation = create(:reservation, restaurant: @restaurant)
-    refute @restaurant.available?(97, @start_time )
+    reservation = create(:reservation, restaurant: @restaurant, start_time: @start_time)
+    party_size_to_be_tested  = @restaurant.seating_capacity - reservation.partysize + 1 
+    refute @restaurant.available?(party_size_to_be_tested, @start_time )
   end
 
-  test "reservation is available when reservations plus party size are at different times" do
-    reservation = create(:reservation, restaurant: @restaurant)
-    future_res_date = @start_time + 3.weeks
-    future_party_size = @restaurant.seating_capacity
-    assert @restaurant.available?(future_party_size, future_res_date)
+  test "reservation is available when reservations + party size are at different times" do
+    reservation = create(:reservation, restaurant: @restaurant, start_time: @start_time)
+    party_size_to_be_tested  = @restaurant.seating_capacity - reservation.partysize + 1 
+    assert  @restaurant.available?(party_size_to_be_tested, @start_time + 3.weeks )
   end
-  
 end
